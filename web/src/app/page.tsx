8fcbd0a55f9add88da4f4b05b1a1442af9b45830
Riho Pihlak <riho@devtailor.com>
2023-09-15 13:01:40 +0300

    Add story paragraphs

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,6 +1,18 @@
 export default function Home() {
+  const stories = ['Placeholder'];
+
   return (
     <div className="container">
+      {
+        stories.map((story, index) =>
+          <div className="row" key={`${index}`}>
+            <div className="col-sm-6 mx-auto">
+              <p>{story}</p>
+            </div>
+          </div>
+        )
+      }
+
       <div className="row">
         <div className="col-sm-6 d-flex justify-content-center mx-auto">
           <button className="btn btn-primary">Generate</button>
