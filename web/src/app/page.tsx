aef051d30d0be6d168041585e03768da364a7559
Riho Pihlak <riho@devtailor.com>
2023-09-15 13:32:29 +0300

    Implement generating story

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,5 +1,15 @@
+'use client';
+
+import { useState } from "react";
+
 export default function Home() {
-  const stories = ['Placeholder'];
+  const [stories, setStories] = useState<string[]>([]);
+
+  const genStory = async () => {
+    const response = await fetch('http://localhost:3000/gen-story', { method: 'POST' });
+    const body = await response.json();
+    setStories([...stories, body.story]);
+  };
 
   return (
     <div className="container">
@@ -15,7 +25,7 @@ export default function Home() {
 
       <div className="row">
         <div className="col-sm-6 d-flex justify-content-center mx-auto">
-          <button className="btn btn-primary">Generate</button>
+          <button className="btn btn-primary" onClick={genStory}>Generate</button>
         </div>
       </div>
     </div>
