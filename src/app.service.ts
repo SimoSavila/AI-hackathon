d22e0c7956af560b63d8457121926aa43b9eb3f0
sisavi <108829594+SimoSavila@users.noreply.github.com>
2023-09-15 10:33:26 +0300

    add more test data

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,12 +1,6 @@
 import { HttpService } from '@nestjs/axios';
 import { Injectable } from '@nestjs/common';
-import {
-  CHARACTERS,
-  Episode,
-  madHares,
-  psychoKittiesBackgroundInfo,
-  psychoMollieBackgroundInfo,
-} from './data';
+import { CHARACTERS, MHInfo, PKInfo, PMInfo, shortSummary } from './data';
 import { firstValueFrom } from 'rxjs';
 
 @Injectable()
@@ -14,32 +8,41 @@ export class AppService {
   constructor(private readonly httpService: HttpService) {}
 
   async chatGPT(): Promise<string> {
+    const content = JSON.stringify({
+      backgroundInfo: [PKInfo, MHInfo, PMInfo],
+      characters: CHARACTERS,
+      instructions: `Dont create new charaters!`,
+    });
+
+    const requestData = {
+      model: 'gpt-4', // Adjust the model name as per the latest model from OpenAI
+      messages: [
+        {
+          role: 'system',
+          content: content,
+        },
+        {
+          role: 'user',
+          content: `Generate a new story for me like this: ${shortSummary}`,
+        },
+      ],
+    };
+
     try {
       const result = await firstValueFrom(
         this.httpService.post(
           'https://api.openai.com/v1/chat/completions',
-          {
-            model: 'gpt-4',
-            messages: [
-              {
-                role: 'system',
-                content: `Here is some PK backstory: ${psychoKittiesBackgroundInfo}, PM backstory: ${psychoMollieBackgroundInfo}, MH backstory: ${madHares}, here are some characters ${CHARACTERS}. Do not create any new characters.`,
-              },
-              {
-                role: 'user',
-                content: `Generate new story me like this ${Episode}`,
-              },
-            ],
-          },
+          requestData,
           {
             headers: {
-              Authorization: `Bearer <CHATGPT_TOKEN_GOES_HERE>`,
+              Authorization: `Bearer <API KEY>`,
             },
           },
         ),
       );
 
       console.log(result.data.choices[0].message.content);
+      console.log(result.data.error);
 
       return result.data.choices[0].message.content;
     } catch (error) {
--- src/data.ts
+++ src/data.ts
@@ -10,14 +10,12 @@ export const CHARACTERS: Character[] = [
   {
     name: 'The Time Traveler',
     type: 'PK',
-
     description:
       "This kitty possesses the unique ability to navigate through time with precision. Constantly jumping from one era to another, it's very aware of the historical contexts and uses this knowledge for its advantage.",
   },
   {
     name: 'The Stealth Operator',
     type: 'PK',
-
     description:
       'A master of disguise and blending in, this kitty is able to infiltrate the most secure locations without detection. Its skills make it perfect for undercover operations.',
   },
@@ -136,6 +134,8 @@ With their exceptional mental powers, lack of moral restraint, and unpredictabil
 Conflicts and power struggles among the PsychoKitties spanned centuries, causing significant alterations to humanity's past and future. The United World Encyclopedia documented the emergence of an opposition group driven by jealousy, dedicated to toppling their power-hungry counterparts.
 As the history and future of mankind continue to be rewritten daily, an opportunity arises for individuals to enter the metaverse and assume the role of a formidable PsychoKitty, wielding extraordinary mind-control abilities and influencing the course of history.`;
 
+export const PKInfo = `In the year 2045, the global gene editing market has soared to $124.6 billion, while people in developed countries are immersed in virtual reality. Biotech company GEMX9, led by Antonius Gladwell, seeks to unlock the human brain's potential. Gladwell believes humanity lives in a higher-dimensional computer simulation and aims to break free with quantum-level thinking. GEMX9's experiments with an "Intelligence Gene" on kittens lead to their mysterious disappearance in 2046. Years later, these kittens, now known as "PsychoKitties," reappear with mind-control abilities, altering history, and causing conflicts. An opposition group rises to challenge them, and individuals can enter the metaverse to become powerful PsychoKitties, shaping the course of history.`;
+
 export const psychoMollieBackgroundInfo = `It's been years since PsychoKitties escaped from the GEMX9 lab to start their world takeover journey through time and space.
 Whilst the history and the future are still changing everyday and new powerful positions being attained, the PsychoKitties have realized a deficiency... A lack of sorts that is holding them back from reaching their maximum potential.
 PsychoKitties have figured out that if they surround themselves with any feminine energy on Earth, their inner drive, ambition and mind-bending powers increase.
@@ -146,6 +146,9 @@ The PsychoMollies were born, but they were completely out of control...
 Utility: IP rights, family advantage in Quantum Frenzy game if staked with the right baby and PsychoKitty. More to come.
 Statistics: 10,000 unique collectibles generated from 105 traits`;
 
+export const PMInfo =
+  'Years after their initial escapade, the PsychoKitties discover that surrounding themselves with feminine energy enhances their powers. In pursuit of compatible female partners to unlock new abilities, they travel back to 2045 and steal research on gene modification and kitten embryos. This leads to the creation of the uncontrollable "PsychoMollies," setting the stage for further adventures and intrigue in their quest for power and influence.';
+
 export const madHares = `It's the year 3419 in Rozkazan, known as the most notorious fortress on an ice-covered planet called Abanleor, which imprisons vicious, disturbed and dangerous modified creatures behind bars for the safety of the Milky Way Galaxy.
 iceberg
 Most known villains, called the Mad Hares, were captured within the last 3 centuries and placed in the darkest, most guarded rooms of the facility.
@@ -153,6 +156,9 @@ Since capture, The Universal Intelligence Agency (UIA) has craved to weaponize t
 The final test experiments resulted in a 5% kill rate of innocent subjects in test environments, down 95% from destroying everyone in their way.
 With the increasing tension caused by time-traveling PsychoKitties, a multi-planetary governmental voting took place to send Mad Hares after the mind-controlling felines as the last resort.
 A decision with as high potential consequences as releasing the Mad Hares also required universal decentralized voting to take place. With over 73,299,101 votes and 61% of population siding with the UIA mission, the Mad Hares were equipped with a time-traveling atom-weapon with a single objective in mind - to destroy the PsychoKitties.`;
+
+export const MHInfo =
+  'In the year 3419, on the ice-covered planet Abanleor, the fortress of Rozkazan houses notorious villains known as the Mad Hares. The Universal Intelligence Agency (UIA) seeks to weaponize them by manipulating their brains. Despite progress, the Mad Hares are not fully controllable, with a 5% kill rate in experiments. With tensions rising due to time-traveling PsychoKitties, a multi-planetary vote decides to send the Mad Hares after the felines. Over 73 million votes, with 61% in favor, lead to equipping the Mad Hares with a time-traveling atom-weapon to destroy the PsychoKitties, a decision of potentially immense consequences.';
 
 export const Episode = `
 Story arc plot synopsis:
@@ -377,3 +383,204 @@ MH: Keep searching!
 CAPTION: Alternate endings available!
 
 `;
+
+
+export const shortSummary = `Episode #7:
+MH 1: (in a dim-lit room) The humans have hidden a legendary item imbued with terrible powers. Who gets it can win the war between Psychokitties, PsychoMollies, and Mad Hares!
+
+[The scene shifts to a mysterious-looking box traveling in a time warp.]
+
+CAPTION: The legendary item has been locked into a special box and thrown into a time warp to an unknown destination.
+
+[Back in the dim-lit room, three time warps open behind the Mad Hares.]
+
+MH 1: (excitedly) Let's go!
+
+MH 2: (determined) We must find it before our enemies!
+
+MH 3: (apathetic) Yeah...
+
+[The time warps where MH 1 and MH 2 jumped close.]
+
+MH 3: (removing his Hare mask, revealing himself as an undercover PK) ...good luck with that.
+
+Episode #8:
+[The undercover PK from Episode #7 is in bed with a beautiful PM.]
+
+PK: So there is this special box lost in time, containing the key to win the War. Those stupid Mad Hares think we don't know about it!
+
+[The screen splits into three parts, each showing a PK getting out of a time warp in different moments in history.]
+
+CAPTION: My fellow Psychokitties are searching everywhere. I'm sure we'll be faster than the Mad Hares...
+
+[Back in the bedroom, the PK and PM hold hands, looking at each other lovingly.]
+
+PK: ...but we cannot let the PsychoMollies know. I know I can trust you with my life.
+
+PM: My love, of course you can trust me...
+
+[The PK is suddenly killed, with a big knife piercing his heart. The PM sits on the bed, typing on a smartphone.]
+
+PM: (continues) ...with your life.
+
+Episode #9:
+[A strong MH, holding a laser sword, has won against many gladiators in a fight in the Coliseum.]
+
+CAPTION: Roman Empire, 180 A.D.
+
+MH: Oh great Emperor Commodus! I have killed all of my opponents, and I ask for the Box you own as a prize.
+
+[A lion enters the Coliseum arena.]
+
+EMPEROR COMMODOUS (OFF): I'll give you the Box after a final task, MH.
+
+MH: (confident) A lion? A kid's job for my laser sword...
+
+[The lion shoots a laser from its mouth, killing the MH.]
+
+MH: (shocked) ZOT!!!
+
+[The Emperor Commodus (revealed to be a PK dressed as a Roman Emperor) and the lion (another PK in disguise) stand over the dead MH, laughing.]
+
+Lion: ...unless I use a laser cannon.
+
+Episode #10:
+[A rocket approaches the Moon.]
+
+CAPTION: The Moon, 1969
+
+VOICE FROM ROCKET: According to our sources, the Box should be there.
+
+[A PK astronaut steps out of the rocket, about to set foot on the Moon.]
+
+PK: One small step for a Psychokitty...
+
+[Two PMs suddenly emerge from hiding under the Moon's surface, startling the PK.]
+
+[One of the PMs hits the PK's oxygen tank with a knife.]
+
+PK: EEEEK!
+
+[The PK, propelled by escaping oxygen, drifts into deep space.]
+
+PK: EEEEEEEEEeeeeeeeeek.....
+
+PM 1 (to PM 2): Now THAT's a giant leap for Psychomollies!
+
+Episode #11:
+[Two MHs happily dig the Box out of a cave, watched by strange-looking trilobites with Bitcoin eyes. A Velociraptor hides behind a stalagmite, also observing.]
+
+CAPTION: We've found the box in a weird mine in the Jurassic Age...
+
+[One MH holding the Box is attacked by the Velociraptor while a Megalosaurus's head enters the cave, witnessing the scene.]
+
+CAPTION: Unfortunately, my comrade has been eaten by a Velociraptor...
+
+[The Velociraptor is attacked by the Megalosaurus, and the Megalosaurus is about to be bitten by a T-rex.]
+
+CAPTION: ...which has been eaten by a Megalosaurus... which has been eaten by a T-rex!
+
+CAPTION 2: So what else could I do?
+
+[Two MHs, looking shocked and funny, stare at an incredibly fat MH (the survivor).]
+
+FAT MH: ...I ate the T-rex.
+
+Episode #12:
+[The same scene continues from Episode #11. The two MHs who heard Fat MH's story look at him with evil intent.]
+
+MH 1: Sorry, bro, if you ate the Box we're looking for... I'm afraid the only solution is to rip you open and get it.
+
+[The two MHs, holding a laser knife and a buzzsaw, approach the Fat MH, who is trembling.]
+
+FAT MH: Oh my God! You cannot do that! I can't escape! I cannot MOVE!!!
+
+[Out of panic, the Fat MH vomits, covering the other MHs with puke, including dinosaur bones, MH bones, and the Box.]
+
+FAT MH: BLEEEEURGH
+
+[The Fat MH, now much thinner and flushed, looks at the others covered in puke. They are clearly shocked and disgusted, even though one of them is now holding the Box.]
+
+FAT MH (embarrassed): Hey, the good news is, there was another solution.
+
+Episode #13:
+[The same scene continues from Episode #12. The two MHs who just heard Fat MH's story are looking at him with evil intent.]
+
+MH 1: (holding a laser knife) Sorry, bro, if you ate the Box we're looking for... I'm afraid the only solution is to rip you open and get it.
+
+[The two MHs, now holding a laser knife and a buzzsaw, approach the Fat MH, who is trembling.]
+
+FAT MH: Oh my God! You cannot do that! I can't escape! I cannot MOVE!!!
+
+[Out of panic, the Fat MH vomits, covering the other MHs with puke, including dinosaur bones, MH bones, items, and the Box.]
+
+FAT MH: BLEEEEURGH
+
+[The Fat MH, now much thinner and flushed, looks at the others covered in puke. They are clearly shocked and disgusted, even though one of them is now holding the Box.]
+
+FAT MH (embarrassed): Hey, the good news is, there was another solution.
+
+Episode #13 END A:
+[The Box is open, and the PK takes out a shining Roman Crown.]
+
+PM: OOOOH!
+
+MH: A Roman Crown!
+
+[The PK puts the Crown on his head, and it shoots two lightning bolts, killing the MH and PM.]
+
+PK: The one who wears it will rule them all!
+
+[The PK raises an arm in triumph. We can see that the arm is severed from the previous fight and is losing a lot of blood.]
+
+PK (happy): I won! I am the King!
+
+[The PK, now looking sick, sadly looks at his own severed arm.]
+
+PK: A pity there's no one left to rule before I die myself.
+
+Episode #13 END B:
+[The Box is open, and the PM takes out an Astronaut Helmet.]
+
+PM: Astronaut helmet...
+
+MH (taking out a knife): Who wears it will rule!
+
+[The MH, PK, and PM all strike each other simultaneously.]
+
+MH: It's mine!
+
+PM: No, mine!
+
+PK: AAARGH!
+
+[The three badly wounded characters die on the floor, with the Astronaut Helmet in the middle.]
+
+[A ticket is attached to the helmet, and we can now read it. It says "Made in China."]
+
+CAPTION: Alternate endings available!
+
+Episode #13 END C:
+[The Box is open, and the MH takes out the Bitcoin Eyes.]
+
+MH: Bitcoin Eyes!
+
+[The PK and PM look at the MH, puzzled, while he throws the Bitcoin Eyes at them with one hand and rips his own eyes out with the other hand.]
+
+PK: What shall we do with them?
+
+MH (ripping his own eyes out): Replace my eyes!
+
+[Both the PK and the PM also rip their eyes out while the Bitcoin Eyes fall on the floor.]
+
+PK: No, replace my eyes!
+
+PM: No, replace my eyes!
+
+[The MH, PK, and PM, all of them blind and losing a lot of blood, search the floor with their hands.]
+
+PM: Great. Cannot find them now.
+
+MH: Keep searching!
+
+CAPTION: Alternate endings available!`;
